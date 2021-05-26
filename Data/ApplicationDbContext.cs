304dc6523a41cd6ad60aea674dddf42108b068d5
Mark <spammailgg25@gmail.com>
2021-05-26 12:38:52 +0200

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DockerApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DockerApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DockerApi.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
